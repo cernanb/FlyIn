ad2694b79e79967e1679785fbbb9f58d59df7e59
MilanP5G <milan_parmar@hotmail.co.uk>
2020-07-28 13:37:01 +0100

    created fetch function for Countries

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,15 @@
+const BASE_URL = "http://localhost:3000/"
+
 document.addEventListener("DOMContentLoaded", () => {
 })
+
+function fetchCountries(){
+  fetch(`${BASE_URL}/countries`)
+  .then(resp => resp.json())
+  .then((json) => renderCountries(json))
+}
+
+function renderCountries(countries){
+  
+
+}
